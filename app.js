90eee7aea9d3750d849941e0f9738a916a6b6276
unknown <1760814986@qq.com>
2020-11-23 16:47:32 +0800

    基础测试使用完成

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 App({
   // 1 应用第一次启动的就会触发的事件
   onLaunch: function () {
-    console.log("onLaunch");
+    // console.log("onLaunch");
+    // 出错的时候不能触发 onPageNotFound 事件;
+    // wx.wx.navigateTo({
+    //   url: '/11/22/33'
+    // });
+      
     
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -38,7 +43,35 @@ App({
   },
   // 2 应用 被用户看到
   onShow(){
+    // 对应用的数据或者页面效果做重置
     console.log("onShow");
+  },
+  // 3 应用 被隐藏了
+  onHide(){
+    // 暂停或者清除定时器；
+    console.log("Hide");
+  },
+  // 4 应用的代码发生了报错的时候 就会触发
+  onError(err){
+    // 在应用发生代码报错的时候，收集用户的错误信息，通过异步请求 将错误的信息发送到后台去
+    console.log("onError");
+    console.log(err);
+  },
+  // 4 应用的代码发生了报错的时候 就会触发
+  onError(err){
+    // 在应用发生代码报错的时候，收集用户的错误信息，通过异步请求 将错误的信息发送到后台去
+    console.log("onError");
+    console.log(err);
+  },
+  // 5 页面找不到的时候 就会触发
+  // 应用第一次启动的时候，如果找不到第一个入口页面 才会触发
+  onPageNotFound(){
+    // 如果页面不存在了 通过js的方式来重新跳转页面 重新跳转到第二个首页
+    console.log("onPageNotFound");
+    // 不能跳到tabbar页面
+    wx.wx.navigateTo({
+      url: '/pages/demo/demo'
+    });
   },
   globalData: {
     userInfo: null
--- pages/basicLabel/basicLabel.js
+++ pages/basicLabel/basicLabel.js
@@ -26,7 +26,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("onLoad");
+    // onLoad发送异步请求来初始化页面数据
   },
 
   /**
@@ -45,13 +46,15 @@ Page({
 
   /**
    * 生命周期函数--监听页面隐藏
+   * 页面跳转会触发当前页面隐藏
    */
   onHide: function () {
 
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 生命周期函数--监听页面卸载 也可以通过点击超链接来演示
+   * 带"关闭"俩字儿的时候都会卸载
    */
   onUnload: function () {
 
@@ -61,20 +64,46 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh')
+    // 页面的数据 或者效果 重新 刷新
   },
 
   /**
    * 页面上拉触底事件的处理函数
+   * 需要让页面 出现上下滚动才行
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom')
+    // 上拉加载下一页数据
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+
+  /**
+   * 页面滚动 就可以触发
+   */
+  onPageScroll: function () {
+    
+  },
+
+  /**
+   * 页面的尺寸发生改变的时候 触发
+   * 小程序发生了，横屏竖屏切换的时候触发(在json文件里面配置)
+   */
+  onResize: function () {
+
+  },
+
+  /**
+   * 1 必须要求当前页面 也是tabbar页面
+   * 2 点击的自己的tab item的时候才触发
+   */
+  onTabItemTap: function () {
 
   }
 })
